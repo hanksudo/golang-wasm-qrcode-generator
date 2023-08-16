4f3246221f77707fcb91001ed8a4b1915949aced
Hank Wang <467745+hanksudo@users.noreply.github.com>
2023-08-16 15:18:57 +0900

    fix: generator

--- main.go
+++ main.go
@@ -5,17 +5,31 @@ package main
 //go:generate cp $GOROOT/misc/wasm/wasm_exec.js .
 
 import (
+	"encoding/base64"
+	"syscall/js"
+
 	"github.com/skip2/go-qrcode"
 )
 
-func GenerateQRCode(input string) ([]byte, error) {
-	png, err := qrcode.Encode(input, qrcode.Medium, 256)
+func generateQRCode(this js.Value, p []js.Value) interface{} {
+	input := p[0].String()
+	qr, err := qrcode.New(input, qrcode.Medium)
 	if err != nil {
-		return nil, err
+		return nil
 	}
-	return png, nil
+
+	// Generate the QR code image as a PNG
+	qrImage, err := qr.PNG(256)
+	if err != nil {
+		return nil
+	}
+
+	qrCodeBase64 := base64.StdEncoding.EncodeToString(qrImage)
+	return js.ValueOf(qrCodeBase64)
 }
 
 func main() {
-
+	ch := make(chan struct{})
+	js.Global().Set("generateQRCode", js.FuncOf(generateQRCode))
+	<-ch
 }
